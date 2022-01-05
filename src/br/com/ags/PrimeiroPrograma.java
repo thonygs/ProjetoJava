bd6e0e107a0e04580ecd53ce4b3cbd2b706d9740
Anthony Silva <anthonygabriel182004@gmail.com>
2022-01-05 10:14:09 -0400

    first commit

---
+++ src/br/com/ags/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.ags;
+
+import br.com.ags.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro();
+        System.out.println(cachorro);
+//        int a = 8;
+//        int b = 10;
+//        System.out.println("Hello World! " + (a+b));
+    }
+}
